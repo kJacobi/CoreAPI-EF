35e7790197abad2e5c35d67b09fbfd92fd00c7c5
kJacobi <kennethmjacobi@gmail.com>
2020-02-23 17:08:52 -0500

    Add EF Core and Swagger

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CoreAPI_EF
 {
@@ -26,6 +30,17 @@ namespace CoreAPI_EF
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(x => {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "EF Core API", Version = "v1" });
+
+                // Used only if adding XML comments /remarks to swagger methods.
+                // Also requires right click on projects and go to build tab, check "XML documentation file" checkbox.
+                // Use
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //x.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +50,12 @@ namespace CoreAPI_EF
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var swaggerOptions = new Options.SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            app.UseSwagger(option => { option.RouteTemplate = swaggerOptions.JsonRoute; });
+            app.UseSwaggerUI(option => { option.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.Description); });
 
             app.UseHttpsRedirection();
 
